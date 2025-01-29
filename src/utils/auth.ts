4e8c4293a3fd3f461ebe67977d0b4a664742e0c2
Bobert <salvo.robertroy@gmail.com>
2025-01-29 20:29:45 +0800

    refactor: edited auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,40 +1,47 @@
 // Authentication and cookie management utilities
 
+// Check if code is running on the client-side
+const isClient = () => typeof window !== "undefined";
+
 // Check if user has a valid session
 export const checkSession = (): boolean => {
-  const sessionToken = getCookie('sessionToken');
-
-  return !!(sessionToken);
+  if (!isClient()) return false;
+  const sessionToken = getCookie("sessionToken");
+  return !!sessionToken;
 };
 
 // Get user session data
 export const getSessionUser = () => {
-  if (!checkSession()) return null;
+  if (!isClient() || !checkSession()) return null;
   
   return {
-    userID: getCookie('uniqueID'),
-    username: getCookie('username'),
-    sessionToken: getCookie('sessionToken')
+    userID: getCookie("uniqueID"),
+    username: getCookie("username"),
+    sessionToken: getCookie("sessionToken"),
   };
 };
 
 // Cookie utility functions
 export const setCookie = (name: string, value: string, days: number): void => {
+  if (!isClient()) return;
+  
   const date = new Date();
   date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
   document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/; SameSite=Lax`;
 };
 
 export const getCookie = (name: string): string | undefined => {
-  const cookies = document.cookie.split('; ');
-  const cookie = cookies.find(c => c.startsWith(`${name}=`));
-  return cookie ? cookie.split('=')[1] : undefined; 
+  if (!isClient()) return undefined;
+  
+  const cookies = document.cookie.split("; ");
+  const cookie = cookies.find((c) => c.startsWith(`${name}=`));
+  return cookie ? cookie.split("=")[1] : undefined;
 };
 
-
 export const clearSession = (): void => {
-  // Set cookies to expire immediately
-  document.cookie = 'sessionToken=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-  document.cookie = 'userID=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-  document.cookie = 'username=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+  if (!isClient()) return;
+  
+  document.cookie = "sessionToken=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+  document.cookie = "userID=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+  document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
 };
